2fdfe8b4b617f01c5e38335ba681248a93898d9d
Harlan Cleiton <harlancleiton@gmail.com>
2020-07-04 19:53:14 -0300

    chore: run all tests before pushing updates

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@ module.exports = {
     'pre-commit': 'lint-staged',
     'prepare-commit-msg': 'exec < /dev/tty && git-cz --hook || true',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-push': 'yarn run test',
+    'pre-push': 'yarn run test:all',
   },
 };
